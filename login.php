e4ac61aab17d81647124d0c3b8fa058dc83be888
Jethro QGC <jethrodelima.qgc@gmail.com>
2025-06-17 05:52:37 -0400

    6/17/2025

--- login.php
+++ login.php
@@ -12,6 +12,34 @@ if (!empty($_POST['username']) && !empty($_POST['password'])) {
 
   if ($query->num_rows < 1) {
     $_SESSION['error'] = 'Cannot find account with that username';
+<<<<<<< Updated upstream
+=======
+    header('Location: index.php');
+    exit;
+  }
+
+  $row = $query->fetch_assoc();
+
+  if (password_verify($password, $row['password'])) {
+    $_SESSION['user'] = $row['id'];
+
+    // Role-based redirection using actual DB username
+    $actualUsername = $row['username'];
+    if (strpos($actualUsername, 'source') === 0) {
+      header('Location: dashboard_source.php');
+    } elseif (strpos($actualUsername, 'recruiter') === 0) {
+      header('Location: dashboard_recruiter.php');
+    } elseif (strpos($actualUsername, 'trainer') === 0) {
+      header('Location: dashboard_trainer.php');
+    } elseif (strpos($actualUsername, 'admin') === 0) {
+      header('Location: dashboard_admin.php');
+    }
+    else {
+      header('Location: index.php');
+    }
+    exit;
+
+>>>>>>> Stashed changes
   } else {
     $row = $query->fetch_assoc();
     if (password_verify($password, $row['password'])) {
